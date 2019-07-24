1c37e940f2b527b90e66611a72cf7bd3c91e8a6e
Andrey Sidorov <takandar@gmail.com>
2019-07-24 19:03:49 +0300

    select styles

--- src/components/select.vue
+++ src/components/select.vue
@@ -1,8 +1,15 @@
 <template>
   <div class='n1-select' :class='{ expanded: isExpanded }'>
-    <button class='label' @click='toggle'>
-      {{ options[value] }}
-    </button>
+    <div class='outline' :class='{ focused: isFocused || isExpanded }'>
+      <button
+        class='label'
+        @click='toggle'
+        @focus='focus'
+        @blur='blur'
+      >
+        {{ options[value] }}
+      </button>
+    </div>
   </div>
 </template>
 
@@ -15,11 +22,18 @@ export default {
     value: { type: String, required: false, default: undefined }
   },
   data: () => ({
-    isExpanded: false
+    isExpanded: false,
+    isFocused: false
   }),
   methods: {
     toggle() {
       this.isExpanded = !this.isExpanded;
+    },
+    focus() {
+      this.isFocused = true;
+    },
+    blur() {
+      this.isFocused = false;
     }
   }
 };
@@ -27,43 +41,61 @@ export default {
 
 <style lang='sass'>
 .n1-select
-  background: #fff
   position: relative
 
-  &:first-child .label
-    border-top-left-radius: 4px
-    border-bottom-left-radius: 4px
+  &:first-child
+    .outline
+      border-top-left-radius: 5px
+      border-bottom-left-radius: 5px
 
-  &:last-child .label
-    border-top-right-radius: 4px
-    border-bottom-right-radius: 4px
+    .label
+      border-top-left-radius: 4px
+      border-bottom-left-radius: 4px
+
+  &:last-child
+    .outline
+      border-top-right-radius: 5px
+      border-bottom-right-radius: 5px
+
+    .label
+      border-top-right-radius: 4px
+      border-bottom-right-radius: 4px
 
   &.expanded .label:before
     transform: translateY(-50%) rotate(180deg)
 
-  .label
-    border: 1px solid #d9d9d9
-    box-sizing: border-box
-    display: block
-    cursor: pointer
-    font-size: 17px
-    line-height: 36px
-    padding: 0 12px
-    position: relative
-    padding-right: 36px
+.outline
+  border: 3px solid transparent
 
-    &:before
-      background-image: url('../assets/select/arrow.svg')
-      background-position: center
-      background-repeat: no-repeat
-      content: ''
-      height: 20px
-      position: absolute
-      right: 8px
-      transform: translateY(-50%)
-      transition: transform 0.25s ease
-      top: 50%
-      width: 20px
+  &.focused
+    border-color: #b3ddff
+    background: #b3ddff
+
+.label
+  background: #fff
+  border: 1px solid #d9d9d9
+  box-sizing: border-box
+  cursor: pointer
+  display: block
+  font-size: 17px
+  line-height: 36px
+  outline: none
+  overflow: hidden
+  padding: 0 36px 0 12px
+  position: relative
+
+  &:before
+    background-image: url('../assets/select/arrow.svg')
+    background-position: center
+    background-repeat: no-repeat
+    content: ''
+    height: 20px
+    position: absolute
+    right: 8px
+    transform: translateY(-50%)
+    transition: transform 0.25s ease
+    top: 50%
+    width: 20px
 
 .n1-select + .n1-select
   border-left: none
